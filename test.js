d59a628969afcf748e640739d6b5dce91525b651
Javier Gutierrez (Yav Gz) <gutierrez.alcantara.javier@gmail.com>
2022-10-23 21:51:45 -0500

    User name test

---
+++ test.js
@@ -0,0 +1,18 @@
+let name = "Javier"
+let secondName = "Gutierrez"
+let nickName = "Yav_Gz"
+let age = 24
+let email = "yaav.gs@gmail.com"
+let older = true
+let cashSave = 2000
+let debts = 7500
+
+let input1 = document.querySelector(".user")
+
+
+
+function print () {
+  input1.innerHTML = `Usuario: ${name} ${secondName}`
+}
+
+print();

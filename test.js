6a75c71c5d2221939e2ac38880e8a740bf3a0965
Javier Gutierrez (Yav Gz) <gutierrez.alcantara.javier@gmail.com>
2022-11-12 20:46:02 -0600

    condicionales

--- test.js
+++ test.js
@@ -57,7 +57,30 @@ function print(a, b, c) {
 
 
 
-// function printName2() {
-  // div2.appendChild(p)
-  
-// }
+// Replica el comportamiento del siguiente código que usa la sentencia switch utilizando if, else y else if:
+const tipoDeSuscripcion = "ExpertPlus";
+
+switch (tipoDeSuscripcion) {
+   case "Free":
+       console.log("Solo puedes tomar los cursos gratis");
+       break;
+   case "Basic":
+       console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+       break;
+   case "Expert":
+       console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+       break;
+   case "ExpertPlus":
+       console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+       break;
+}
+
+if (tipoDeSuscripcion === "Free") {
+  console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion === "Basic") {
+  console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+} else if (tipoDeSuscripcion === "Expert"){
+  console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+}else{
+  console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+}
